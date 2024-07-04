6de154f949de299264b0d7e905cc370159111663
blue-jay3 <emferra01@gmail.com>
2024-07-03 21:19:34 -0400

    fixed a bunch of bugs, implemented total balance counter, can add and subtract money from the balance, trash button to remove an expense, additional styling

--- main.js
+++ main.js
@@ -1,17 +1,55 @@
 function addMoney() {
-    searchBar = document.getElementById('search-bar');
-    searchValue = searchBar.value;
+    searchBar = document.getElementById('search-bar')
+    searchValue = searchBar.value
 
-    expenseList = document.getElementById('expense-list');
+    if(searchBar.value == NaN || searchBar.value.trim() == "" || isNaN(searchBar.value)) {
+        return
+    }
 
-    expenseItem = document.createElement("li");
-    expenseItem.className = 'expense-item';
+    expenseList = document.getElementById('expense-list')
+
+    expenseItem = document.createElement("li")
+    removeIcon = document.createElement("i")
+    removeIcon.classList.add('bx')
+    removeIcon.classList.add('bx-trash')
+    removeIcon.onclick = function() {this.parentNode.remove(); updateTotal(parseFloat(this.parentNode.innerHTML)*-1);}
+    expenseItem.className = 'add-expense-item'
 
     expenseItem.innerHTML = searchValue
+    expenseItem.appendChild(removeIcon)
+    updateTotal(parseFloat(searchValue))
 
-    expenseList.appendChild(expenseItem);
+    expenseList.appendChild(expenseItem)
+    searchBar.value = ""
 }
 
 function subtractMoney() {
+    searchBar = document.getElementById('search-bar')
+    searchValue = searchBar.value
 
+    if(searchBar.value == NaN || searchBar.value.trim() == "" || isNaN(searchBar.value)) {
+        return
+    }
+
+    expenseList = document.getElementById('expense-list')
+
+    expenseItem = document.createElement("li")
+    removeIcon = document.createElement("i")
+    removeIcon.classList.add('bx')
+    removeIcon.classList.add('bx-trash')
+    removeIcon.onclick = function() {this.parentNode.remove(); updateTotal(parseFloat(this.parentNode.innerHTML));}
+    expenseItem.className = 'subtract-expense-item'
+
+    expenseItem.innerHTML = searchValue
+    expenseItem.appendChild(removeIcon)
+    updateTotal(parseFloat(searchValue*-1))
+
+    expenseList.appendChild(expenseItem)
+    searchBar.value = ""
+}
+
+function updateTotal(expense) {
+    totalText = document.getElementById('display-total')
+    currentTotal = totalText.innerHTML.slice(1)
+    totalText.innerHTML = "$" + String(parseFloat(currentTotal) + expense)
 }
